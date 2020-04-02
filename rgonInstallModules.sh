65c368b0f0d6e98aafdee0b41adec2f0e7e32353
rgon <rgon@outlook.com>
2020-04-02 12:43:04 +0200

    Easy config script (devel testing tool)

---
+++ rgonInstallModules.sh
@@ -0,0 +1,15 @@
+git clone github.com/rgon/pulseaudio
+cd pulseaudio
+# software-properties-gtk # enable "Source Code"
+sudo apt -y build-dep pulseaudio
+./bootstrap.sh
+make -j 5
+sudo cp src/.libs/module-bluetooth-policy.so /usr/lib/pulse-13.0/modules/module-bluetooth-policy-rgon.so
+
+pulseaudio --dump-modules | grep policy
+pactl unload-module module-bluetooth-policy
+pactl load-module module-bluetooth-policy-rgon
+
+sudo mv /usr/lib/pulse-13.0/modules/module-bluetooth-policy.so /usr/lib/pulse-13.0/modules/module-bluetooth-policy.so.bak
+
+sudo apt remove pulseaudio-module-bluetooth
