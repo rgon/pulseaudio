bdd18dd4a23077efcc376decca928729c0c4bcce
rgon <rgon@outlook.com>
2020-04-02 17:25:29 +0200

    modified install script

--- rgonInstallModules.sh
+++ rgonInstallModules.sh
@@ -1,26 +1,45 @@
-git clone github.com/rgon/pulseaudio
-cd pulseaudio
-# software-properties-gtk # enable "Source Code"
-sudo apt -y build-dep pulseaudio
-./bootstrap.sh
-make -j 5
-sudo cp src/.libs/module-bluetooth-policy.so /usr/lib/pulse-13.0/modules/module-bluetooth-policy-rgon.so
+#!/bin/bash
 
-pulseaudio --dump-modules | grep policy
-pactl unload-module module-bluetooth-policy
-pactl load-module module-bluetooth-policy-rgon
+echo "Please run this script as sudo if executed with the 'install' or 'build' param"
 
-sudo mv /usr/lib/pulse-13.0/modules/module-bluetooth-policy.so /usr/lib/pulse-13.0/modules/module-bluetooth-policy.so.bak
+fn_run () {
+	pacmd unload-module module-bluetooth-policy
+	pacmd unload-module module-jack-sink
+	pacmd unload-module module-loopback
 
-sudo apt remove pulseaudio-module-bluetooth
+	pacmd load-module module-jack-sink sink_name="OSAppRecord"
+	pacmd load-module module-jack-sink sink_name="BTDevRecord"
+	pacmd load-module module-jack-source source_name="OSAppCapture"
+	pacmd load-module module-jack-source source_name="BTDevCapture"
 
-sudo apt remove pulseaudio pulseaudio-utils
+	pacmd load-module module-bluetooth-policy
 
-pacmd describe-module module-bluetooth-policy
-pacmd load-module module-jack-sink
-pacmd load-module module-bluetooth-policy
+	pacmd list-sources | grep jack
+	pacmd list-sinks | grep jack
 
-make clean
-./bootstrap.sh
-make -j 5
-sudo make install
+	pacmd load-module module-bluetooth-policy custom_loopback_sink="BTDevRecord" custom_loopback_source="BTDevCapture"
+}
+
+fn_build () {
+	sudo apt remove pulseaudio pulseaudio-utils
+	make clean
+	./bootstrap.sh
+	make -j 5
+	sudo make install
+}
+
+if [ "$1" == "install" ]; then
+	git clone https://github.com/rgon/pulseaudio
+	cd pulseaudio
+	# software-properties-gtk # enable "Source Code"
+	sudo apt -y build-dep pulseaudio
+
+	fn_build
+elif [ "$1" == "build" ]; then
+	fn_build
+else # run
+	pacmd describe-module module-bluetooth-policy
+	fn_run
+fi
+
+# sudo mv /usr/lib/pulse-13.0/modules/module-bluetooth-policy.so /usr/lib/pulse-13.0/modules/module-bluetooth-policy.so.bak
