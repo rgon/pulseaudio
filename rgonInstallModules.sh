bc0aff5e98322c98e080ca6e32d9775de31d3b9b
rgon <rgon@outlook.com>
2020-04-02 16:10:48 +0200

    updated install script

--- rgonInstallModules.sh
+++ rgonInstallModules.sh
@@ -13,3 +13,14 @@ pactl load-module module-bluetooth-policy-rgon
 sudo mv /usr/lib/pulse-13.0/modules/module-bluetooth-policy.so /usr/lib/pulse-13.0/modules/module-bluetooth-policy.so.bak
 
 sudo apt remove pulseaudio-module-bluetooth
+
+sudo apt remove pulseaudio pulseaudio-utils
+
+pacmd describe-module module-bluetooth-policy
+pacmd load-module module-jack-sink
+pacmd load-module module-bluetooth-policy
+
+make clean
+./bootstrap.sh
+make -j 5
+sudo make install
